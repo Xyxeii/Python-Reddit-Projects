2c99ad95c16f3a278663cf3b9d131e0da83c4fbf
Xyxeii <anthonymartiniano@gmail.com>
2019-10-10 20:40:44 -0400

    nothing important

--- Fibonacci Sequence.py
+++ Fibonacci Sequence.py
@@ -8,9 +8,9 @@ def fib():
     fib_sequence = [0, 1]
     if user_input > 1:
         for x in range(user_input - 1):
-            fib_sequence.append(fib_sequence[x] + fib_sequence[x+1])
+            fib_sequence.append(fib_sequence[x] + fib_sequence[x +  1])
     else:
         return fib_sequence[user_input]
-    return fib_sequence[user_input-1] + fib_sequence[user_input - 2]
+    return fib_sequence[user_input -1 ] + fib_sequence[user_input - 2]
 
 print(fib())
